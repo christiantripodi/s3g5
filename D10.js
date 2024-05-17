1af545b439016073d2cddc6d625484a93bc60a0a
christiantripodi <skl11@icloud.com>
2024-05-17 12:03:48 +0200

    conmillennium

--- D10.js
+++ D10.js
@@ -404,23 +404,55 @@ const movies = [
   Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
 */
 
+let newestMovie = () => {
+  let annoFilm = movies[0].Year;
+  movies.forEach((film) => {
+    if (film.Year > annoFilm) annoFilm = film.Year;
+  });
+  return annoFilm;
+};
 
+console.log(newestMovie())
 
 
 /* ESERCIZIO 13
   Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
 */
 
+function countMovies () {
+  return movies.length
+}
 
-
+console.log(countMovies())
 
 /* ESERCIZIO 14
   Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
 */
 
+let onlyTheYears = () => {
+  let anniFilm = [];
+  anniFilm = movies.map((film) => film.Year);
+  return anniFilm;
+};
+
+console.log(onlyTheYears())
+
+
+
+
 /* ESERCIZIO 15
   Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
 */
+
+
+function onlyInLastMillennium() {
+  return movies.filter((el) => el.Year < 2000);
+
+}
+console.log(onlyInLastMillennium())
+
+
+
 
 /* ESERCIZIO 16
   Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
